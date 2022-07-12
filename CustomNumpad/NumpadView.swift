643703e0e17fd6a3c8948fdf19eb60d4957b669f
artexhibit <vendabal@yandex.ru>
2022-07-12 21:44:08 +0300

    Improvements

--- CustomNumpad/NumpadView.swift
+++ CustomNumpad/NumpadView.swift
@@ -3,14 +3,12 @@ import UIKit
 
 class NumpadView: UIView, UIInputViewAudioFeedback {
     
-    @IBOutlet var buttons: [UIButton]!
+    var target: UITextInput?
+    var enableInputClicksWhenVisible: Bool { return true }
     
-    var enableInputClicksWhenVisible: Bool {
-        return true
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(target: UITextInput) {
+        super.init(frame: .zero)
+        self.target = target
         initializeSubview()
     }
     
@@ -24,5 +22,27 @@ class NumpadView: UIView, UIInputViewAudioFeedback {
         let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)![0] as! UIView
         self.addSubview(view)
         view.frame = self.bounds
+        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    @IBAction func buttonPressed(_ sender: NumpadButton) {
+        insertText((sender.titleLabel!.text)!)
+    }
+    
+    func insertText(_ string: String) {
+        guard let range = target?.selectedRange else { return }
+        if let textField = target as? UITextField, textField.delegate?.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) == false {
+            return 
+        }
+        target?.insertText(string)
+    }
+}
+
+extension UITextInput {
+    var selectedRange: NSRange? {
+        guard let selectedRange = selectedTextRange else { return nil }
+        let location = offset(from: beginningOfDocument, to: selectedRange.start)
+        let length = offset(from: selectedRange.start, to: selectedRange.end)
+        return NSRange(location: location, length: length)
     }
 }
--- CustomNumpad/NumpadViewController.swift
+++ CustomNumpad/NumpadViewController.swift
@@ -2,27 +2,38 @@
 import UIKit
 
 class NumpadViewController: UIViewController, UITextFieldDelegate {
-    
+
     @IBOutlet weak var textField: UITextField!
-    
-    let numpadView = NumpadView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupKeyboardHide()
         textField.delegate = self
-        textField.inputView = numpadView
-        textField.inputView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        textField.inputView = NumpadView(target: textField)
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        return true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = " "
+        formatter.maximumFractionDigits = 2
+
+        let textString = textField.text ?? ""
+        guard let range = Range(range, in: string) else { return false }
+        let updatedString = textString.replacingCharacters(in: range, with: string)
+        let correctDecimalString = updatedString.replacingOccurrences(of: ",", with: ".")
+        let completeString = correctDecimalString.replacingOccurrences(of: formatter.groupingSeparator, with: "")
+
+        guard completeString.count <= 12 else { return false }
+        guard !completeString.isEmpty else { return true }
+        textField.text = completeString
+        
+        return string == formatter.decimalSeparator
     }
 }
-
 
 // Hide Numpad by tap on the screen
 extension NumpadViewController {
