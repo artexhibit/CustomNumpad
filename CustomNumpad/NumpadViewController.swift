fb305e8815eb4aaf31d972e5da4b33e09b1f6f39
artexhibit <vendabal@yandex.ru>
2022-07-23 10:37:11 +0300

    improvements

--- CustomNumpad/NumpadViewController.swift
+++ CustomNumpad/NumpadViewController.swift
@@ -14,72 +14,91 @@ class NumpadViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        var shouldChange: Bool = true
-        var symbol: String?
-        let mathSymbols = "+-x/"
-        //current text visible in the text field
-        let numberString = textField.text ?? ""
-        //last character in the text field
-        let lastCharacter = numberString.last ?? "+"
-        //get all the numbers displayed in the textfield by separating them from symbols
-        let numbersArray = numberString.components(separatedBy: CharacterSet(charactersIn: mathSymbols))
-        
-        if string == "" { return false }
-        //if symbol is a first character in a string - do not insert
-        if numberString == "" {
-            if !Character(string).isNumber { shouldChange = false }
-        }
-        //if a number is entered check for the decimal points
-        else if Character(string).isNumber {
-            if numbersArray.last?.contains(NumberFormatter().decimalSeparator) ?? true && numbersArray.last?.components(separatedBy: NumberFormatter().decimalSeparator).last?.count == 2 && Character(string).isNumber {
-                shouldChange =  false
-            } else {
-                for character in numberString {
-                    if mathSymbols.contains(character){
-                        symbol = String(character)
-                        break
-                    }
-                }
-                if symbol != nil {
-                    textField.text = "\(numbersArray.first?.formatted() ?? "")\(symbol!)\(((numbersArray.last ?? "") + string).formatted())"
-                } else {
-                    textField.text = "\(((numbersArray.first ?? "") + string).formatted())"
-                }
-                shouldChange = false
-            }
-            // if symbol is entered
-        } else {
-            if string == NumberFormatter().decimalSeparator {
-                if !lastCharacter.isNumber { shouldChange = false }
-            }
-            //if there are more than 1 number in numbersArray, calculate the value
-            else if lastCharacter.isNumber {
-                if numbersArray.count > 1 {
-                    let correctDecimalString = numberString.replacingOccurrences(of: NumberFormatter().groupingSeparator, with: "")
-                    let correctString = correctDecimalString.replacingOccurrences(of: NumberFormatter().decimalSeparator, with: ".")
-                    let expression = NSExpression(format: correctString)
-                    let answer = expression.expressionValue(with: nil, context: nil) as! Double
-                    textField.text = "\(answer.formatted())\(string)"
-                    shouldChange = false
-                }
-            } else {
-                //change the symbol
-                textField.text = "\(numberString.dropLast())\(string)"
-                shouldChange = false
-            }
-        }
-        return shouldChange
-    }
-}
-
-extension String {
-    func formatted() -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 1
         formatter.locale = .current
-        let correctDecimalString = self.replacingOccurrences(of: formatter.groupingSeparator, with: "")
-        let correctString = correctDecimalString.replacingOccurrences(of: formatter.decimalSeparator, with: ".")
-        return formatter.string(from: NSNumber(value: Double(correctString) ?? 0)) ?? ""
+        formatter.roundingMode = .down
+        
+        let numberString = "\(textField.text ?? "")".replacingOccurrences(of: formatter.groupingSeparator, with: "")
+        let lastCharacter = numberString.last ?? "."
+        var symbol: String {
+            var tempArray = [String]()
+            let mathSymbols = "+-/x"
+            for character in numberString {
+                if mathSymbols.contains(character) {
+                    tempArray.append(String(character))
+                }
+            }
+            return tempArray.last ?? ""
+        }
+        var numbersArray: [String] {
+            if numberString.first == "-" {
+                let positiveString = numberString.dropFirst()
+                var tempArray = positiveString.components(separatedBy: symbol)
+                tempArray[0] = "-\(tempArray[0])"
+                return tempArray
+            } else {
+                return numberString.components(separatedBy: symbol)
+            }
+        }
+        let amountOfDecimalSigns = "\(numbersArray.last ?? "")\(string)".filter({ $0 == Character(formatter.decimalSeparator) }).count
+        
+        //allow to insert 0 after the decimal symbol to avoid formatting
+        formatter.minimumFractionDigits = numberString.last == Character(formatter.decimalSeparator) && string == "0" ? 1 : 0
+        
+        //allow string to be changed by backspace button
+        if string == "" { return false }
+        
+        //allow numbers as first character, except math symbols
+        if numberString == "" {
+            if Character(string).isNumber {
+                textField.text = string
+            } else {
+                return false
+            }
+        }
+        //allow only one decimal symbol per number
+        else if amountOfDecimalSigns > 1 { return false }
+         
+        if numbersArray.count > 1 {
+        //if number is entered
+            if Character(string).isNumber {
+                textField.text = "\(formatter.string(for: Double("\(numbersArray.first?.replacingOccurrences(of: formatter.decimalSeparator, with: ".") ?? "")") ?? 0 as NSNumber) ?? "")\(symbol)\(formatter.string(for: Double("\(numbersArray.last?.replacingOccurrences(of: formatter.decimalSeparator, with: ".") ?? "")\(string)") ?? 0 as NSNumber) ?? "")"
+        //if symbol is entered
+            } else if string == formatter.decimalSeparator {
+                textField.text = "\(textField.text ?? "")\(string)"
+            } else {
+        //perform calculation if last entered character is number
+                if lastCharacter.isNumber {
+                    let expression = NSExpression(format: numberString.replacingOccurrences(of: formatter.decimalSeparator, with: "."))
+                    let answer =  expression.expressionValue(with: nil, context: nil)
+                    textField.text = "\(formatter.string(from: answer as! NSNumber) ?? "")\(string)"
+        //perform calculation if last entered character is decimal symbol
+                } else if lastCharacter == Character(formatter.decimalSeparator) {
+                    let expression = NSExpression(format: String(numberString.dropLast()).replacingOccurrences(of: formatter.decimalSeparator, with: "."))
+                    let answer = expression.expressionValue(with: nil, context: nil)
+                    textField.text = "\(formatter.string(from: answer as! NSNumber) ?? "")\(string)"
+        //change math symbol before enter a second number
+                } else {
+                    textField.text = "\(textField.text?.dropLast() ?? "")\(string)"
+                }
+            }
+        } else {
+        //if number is entered
+            if Character(string).isNumber {
+                textField.text = "\(formatter.string(for: Double("\(numbersArray.first?.replacingOccurrences(of: formatter.decimalSeparator, with: ".") ?? "")\(string)") ?? 0 as NSNumber) ?? "")"
+            } else {
+        //if math symbol is entered
+                if lastCharacter.isNumber {
+                    textField.text = "\(textField.text ?? "")\(string)"
+                } else {
+                    textField.text = "\(numberString.dropLast())\(string)"
+                }
+            }
+        }
+        return false
     }
 }
 
